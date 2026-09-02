6e77f8b2fafe719ad3f1b4b7dea72f0cbaa4def4
sara2 <sara270498@gmail.com>
2026-09-02 13:17:02 +0300

    fixed

--- test_0.py
+++ test_0.py
@@ -1,7 +1,10 @@
 import re
-from playwright.sync_api import expect
+from playwright.sync_api import Page, expect
 
-def test_googleSearch(page):
+
+def test_googleSearch(page: Page):
+
+  page.wait_for_timeout(3000)
   page.goto("https://www.google.com/ncr") 
 
   try:
@@ -10,6 +13,12 @@ def test_googleSearch(page):
     print("No popup")
 
   page.get_by_role("combobox", name="Search").fill("playwright python")
+  page.keyboard.press("Enter")
+
+  
+  expect(page).to_have_title(re.compile("playwright", re.IGNORECASE))
+  page.pause()
+  
 
 
 
